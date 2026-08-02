7c91c9f80af8ea1d0ee26a8cda71397a18ead911
KaffinPX <KaffinPX@gmail.com>
2026-08-02 04:52:15 +0300

    feat(p2p): Remove deprecated id binding

--- p2p/src/peer/peer_info.rs
+++ p2p/src/peer/peer_info.rs
@@ -1,67 +1,14 @@
 use std::net::IpAddr;
-use std::net::SocketAddr;
 use std::time::SystemTime;
 
 use libp2p::Multiaddr;
-use libp2p::PeerId;
 use libp2p::multiaddr::Protocol;
-use libp2p::multihash::Multihash;
 use serde::Deserialize;
 use serde::Serialize;
-use sha2::Digest;
-use sha2::Sha256;
 
 use super::InstanceId;
 use super::PeerStanding;
 use crate::peer::HandshakeData;
-
-/// Derive a pseudorandom [`PeerId`] from a [`SocketAddr`].
-///
-/// This is a controversial feature, with accordingly complex motivation -- bear
-/// with:
-///
-/// This map is a compatibility layer bridging two architectures. With the
-/// introduction of the libp2p network stack, all peer-related dictionaries
-/// were modified to use the [`PeerId`] as the key instead of the
-/// [`SocketAddr`]. Here is why the [`PeerId`] is a better choice:
-///  - In the libp2p stack, the [`PeerId`] is cryptographically bound to the
-///    peer's public key -- and the stack will refuse to connect if it fails to
-///    verify that the peer really knows the matching secret key.
-///  - The same peer (identified by public key) can have multiple
-///    [`SocketAddr`]s, and even other internet-route-locators. While malicious
-///    attackers can generate many key pairs, the point is that honest peers
-///    will reuse the same public key but will be treated as different peers if
-///    their [`SocketAddr`] is used as a stand-in for their identity. The
-///    [`SocketAddr`] can change under benign circumstances, for instance if the
-///    peer switches from WiFi to 4G, or it their ISP switches to a different
-///    IP. So for honest peers, using the [`PeerId`] as the identifier leads to
-///    less redundant work and greater peer set entropy.
-///  - Malicious nodes must still use a variety of [`SocketAddr`]s if the goal
-///    is to populate a victim's peer set with sybils and drive out honest
-///    peers.Banning still happens at the level of [`SocketAddr`]s. So this
-///    transition does not degrade security.
-///
-/// However, since the legacy peer-to-peer stack has no concept of [`PeerId`],
-/// it is difficult to access the dictionaries that now use the [`PeerId`] as
-/// key, such as `peer_map` and `peer_standing`. This map deterministically
-/// derives an identity ([`PeerId`]) from the peer's [`SocketAddr`]. This is a
-/// controversial identification because a) there is no cryptographic
-/// authentication or even tie to public keys; and b) leads to duplication of
-/// work and weaker peer set entropy. Therefore, this map should only be used in
-/// the context of the legacy peer-to-peer stack and, if the legacy peer-to-peer
-/// stack is deprecated, this function should be deprecated along with it.
-pub fn pseudorandom_peer_id(addr: &SocketAddr) -> PeerId {
-    let mut hasher = Sha256::new();
-    hasher.update(b"legacy-mapping");
-    hasher.update(addr.to_string().as_bytes());
-    let hash_result = hasher.finalize();
-
-    // Use SHA2_256 (Code 0x12) which is the libp2p standard.
-    let mhash = Multihash::wrap(0x12, &hash_result)
-        .expect("SHA2-256 hash length is 32 bytes, which is valid for multihash");
-
-    PeerId::from_multihash(mhash).expect("SHA2-256 is the standard libp2p PeerId hash algorithm")
-}
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub struct PeerConnectionInfo {
